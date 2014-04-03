0d05af3f13c44a7249b37bc90bf95d2fdfe17042
Florian Letsch <florianletsch@gmail.com>
2014-04-03 23:50:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# grey.py
+
+Tiny python script to convert CSS to a greyscale version.
+Just replace html colour values (colour names, 3-digit
+and 6 digit values).
---
+++ grey.py
@@ -0,0 +1,55 @@
+#!/usr/local/bin/python
+
+usage = """
+    Reads css input and prints the CSS as a greyscale version
+
+    Usage:
+    chmod +x grey.py
+    ./grey.py in.css
+    """
+
+import sys
+import json
+import re
+
+def greyscale(css):
+    # fetch all html colour names
+    colours = []
+    with open('colours.json') as json_data:
+        colours = json.load(json_data)
+
+    # not very safe pattern... might capture inside css values
+    colour_names_pattern = "(%s)(?!\w)" % '|'.join(colours.keys())
+    colour_names_regex = re.compile(colour_names_pattern)
+
+    # replace colour names with hex values
+    def replName(m):
+        colour = m.group(1)
+        return colours[colour]['hex']
+    css = re.sub(colour_names_pattern, replName, css, flags=re.I)
+
+    # we only want 6 digit hex values, convert the others
+    css = re.sub("#([a-f\d])([a-f\d])([a-f\d])(?![a-f\d])", r"#\1\1\2\2\3\3", css, flags=re.I)
+
+    # now turn all colours to greyscale
+    def replHex(m):
+        r = int(m.group(1), 16)
+        g = int(m.group(2), 16)
+        b = int(m.group(3), 16)
+
+        grey = (r+g+b)/3
+        return "#%s%s%s" % tuple([hex(grey)[2:4]]*3)
+    css = re.sub("#([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})", replHex, css, flags=re.I)
+
+    return css
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        print usage
+        exit()
+
+    with open(sys.argv[1]) as f:
+        css = f.read()
+
+    sys.stdout.write(greyscale(css))
